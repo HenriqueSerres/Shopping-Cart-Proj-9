88cfc2d70c5ef072c33038048172fbab67d85b7c
HenriqueSerres <henriqueserres@gmail.com>
2021-12-05 19:25:31 -0300

    requisito 9 verdadeiro

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,9 @@
 const idItemUrl = (elemid) => `https://api.mercadolibre.com/items/${elemid}`;
 
 const fetchItem = async (elemid) => {
+  if (elemid === undefined) {
+    throw new Error('You must provide an url');
+  }
   const urlId = idItemUrl(elemid);
   const result = await fetch(urlId);
   const data = await result.json();  
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,8 +2,7 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
+describe('1 - Teste a função fecthProducts', () => {  
   it('se fetchProducts é uma função',() => {
     expect(typeof fetchProducts).toBe('function');
   });
