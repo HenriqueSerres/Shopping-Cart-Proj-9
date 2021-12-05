03e85c0ece68a5d63c51dac2832084210b12be0d
HenriqueSerres <henriqueserres@gmail.com>
2021-12-05 01:09:33 -0300

    requisitos 4, 5, 6 e 7

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const olPai = document.querySelector('.cart__items');
+const computersList = document.querySelector('.items');
+const precoTotal = document.querySelector('.cart');
+const tagTotal = document.createElement('p');
+let total = 0;
+const apagaButton = document.querySelector('.empty-cart');
+tagTotal.className = 'total-price';
+precoTotal.appendChild(tagTotal);
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -25,8 +34,31 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));  
   return section;
 }
+
+const totalPrice = (valor) => {
+  total += valor;
+  tagTotal.innerHTML = total;
+};
+
+const apagaCarrinho = () => {
+  apagaButton.addEventListener('click', () => {
+    // const classTotalPrice = document.querySelectorAll('.cart__item');
+    // classTotalPrice.innerHTML = '';
+    olPai.innerHTML = '';
+    tagTotal.innerHTML = '';
+    localStorage.clear();
+  });
+};
+
+const getPrice = (target, signal) => {
+  const price = target.innerHTML.split('$');
+  totalPrice(signal * (parseFloat(price[1])));
+};
+
 function cartItemClickListener({ target }) {
   target.remove();
+  saveCartItems(olPai.innerHTML);
+  getPrice(target, -1);    
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {  
@@ -39,38 +71,45 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
 
 const criaLista = async (idProduct) => {
   const { id, title, price } = await fetchItem(idProduct);
-  const valores = createCartItemElement({ id, title, price });
-  return valores;
+  const valores = createCartItemElement({ id, title, price });  
+  totalPrice(parseFloat(price));
+  return valores;  
 };
 
 const addListenerButton = () => {
-  const olPai = document.querySelector('.cart__items');
   console.log(olPai);
   const buttons = document.querySelectorAll('.item__add');
   buttons.forEach((button) => button.addEventListener('click', async ({ target }) => {
     const itemId = getSkuFromProductItem(target.parentNode);
     const li = await criaLista(itemId);
     olPai.appendChild(li);
-    saveCartItems(olPai.innerHTML);
+    saveCartItems(olPai.innerHTML);   
   }));  
 };
 
 const getCartItemsLs = () => {
-  const data = getSavedCartItems();
+  const data = getSavedCartItems();  
   const ol = document.querySelector('ol');
   ol.innerHTML = data;
+  ol.querySelectorAll('li').forEach((elem) => {
+    elem.addEventListener('click', cartItemClickListener);
+    getPrice(elem, 1);
+  });  
 };
 
-const elementsItems = async () => {
-  const computersList = document.querySelector('.items');
+const elementsItems = async () => {  
   const data = await fetchProducts('computador');
-  data.forEach((elem) => {
+  computersList.innerHTML = '';
+  data.results.forEach((elem) => {
     computersList.appendChild(createProductItemElement(elem));
   });
 };
 
 window.onload = async () => {  
-  await elementsItems();
+  const spanCarregando = createCustomElement('span', 'loading', 'carregando');
+  computersList.appendChild(spanCarregando);
+  await elementsItems();  
   addListenerButton();
-  getCartItemsLs();  
+  getCartItemsLs(); 
+  apagaCarrinho();
 };
